643c0522e2927a4101b1e92e7ae89b790c16129c
Joplox <68514592+Joplox@users.noreply.github.com>
2021-05-02 11:44:53 -0400

    EnRalidadNoSeQueCambiosTiene

--- Assets/Scripts/UI/ViewInGame.cs
+++ Assets/Scripts/UI/ViewInGame.cs
@@ -5,8 +5,8 @@ using UnityEngine.UI;
 
 public class ViewInGame : MonoBehaviour
 {
-    public Text collectableLabel;
-    //public Text scoreLabel;
+    public Text coinsLabel;
+    public Text timeLabel;
     //public Text maxScoreLabel;
 
     // Update is called once per frame
@@ -15,7 +15,17 @@ public class ViewInGame : MonoBehaviour
         if (GameManager.sharedInstance.currentGameState == GameState.inGame)
         {
             int currentObjects = GameManager.sharedInstance.collectedObjects;
-            collectableLabel.text = currentObjects.ToString();
+            coinsLabel.text = currentObjects.ToString();
         }
+
+        //if (GameManager.sharedInstance.currentGameState == GameState.inGame)
+        //{
+        //    float timeElapsed = Time.time;
+        //    this.timeLabel.text = "Time\n" + timeElapsed.ToString("f0");
+        //}
+
+        //todo esto necesita ser corregido, el tiempo lo tiene que llevar el player y resetearlo cuando muera
+
+
     }
 }
